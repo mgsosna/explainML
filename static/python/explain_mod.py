85d740234863133b951fe9043c2062cfbcc6f90d
Matt Sosna <matt.g.sosna@gmail.com>
2021-04-23 22:08:22 -0400

    Start ExplainMod class

---
+++ static/python/explain_mod.py
@@ -0,0 +1,8 @@
+class ExplainMod:
+    """
+    A class for visualizing and building an intuition in how a given
+    model works.
+    """
+    def __init__(self):
+        self.model = None
+        self.features = None
